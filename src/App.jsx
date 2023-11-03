c0048f478b7a274d1dd626dc97a110e4058bc3f8
monica ran <ranmonica96@gmail.com>
2023-11-03 12:49:25 -0700

    added data functionality and featured puppy functionality

--- src/App.jsx
+++ src/App.jsx
@@ -6,15 +6,28 @@ function App() {
   const [puppies, setPuppies] = useState(puppyList)
   console.log("puppies: ", puppyList);
 
+  const [featPupId, setFeatPupId] = useState(null)
+
+
+  const featuredPup = puppies.find((pup)=> pup.id === featPupId)
+  console.log(featuredPup)
+  
   return (
-      <div className="App"> 
-        {
-          puppies.map((puppy) => {
-            return <p>{puppy.name}</p>
-          })
-        }
-    </div> 
-  );
+    <>
+      {featPupId && (
+        <div class="feat-pup">
+          <h2>{featuredPup.name}</h2>
+          <ul>
+            <li>Age: {featuredPup.age}</li>
+            <li>Email: {featuredPup.email}</li>
+          </ul>
+        </div>
+      )}
+        {puppies.map((puppy) => {
+            return <p onClick={() => { setFeatPupId(puppy.id)}} key={puppy.id}>{puppy.name}</p>;
+        })}
+    </>
+  )
 }
 
 export default App
