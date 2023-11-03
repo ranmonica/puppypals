f3e3f5b37bdf3c2eae758df0a2a7c66267b66330
monica ran <ranmonica96@gmail.com>
2023-11-03 11:44:26 -0700

    added puppy data

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,20 @@
+import { useState } from 'react'
 import './App.css'
+import {puppyList} from './data.js'
 
 function App() {
-  
+  const [puppies, setPuppies] = useState(puppyList)
+  console.log("puppies: ", puppyList);
 
   return (
-    <>
-    </>
-  )
+      <div className="App"> 
+        {
+          puppies.map((puppy) => {
+            return <p>{puppy.name}</p>
+          })
+        }
+    </div> 
+  );
 }
 
 export default App
